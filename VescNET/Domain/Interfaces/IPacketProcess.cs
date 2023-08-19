aa61adc271454b9b6e17f311230f6407e14a938a
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-08-19 12:39:40 -0300

    feat: create PacketProcess interface

---
+++ VescNET/Domain/Interfaces/IPacketProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VescNET.Domain.DTOs;
+
+namespace VescNET.Domain.Interfaces
+{
+    public interface IPacketProcess
+    {
+        ReceivedData Call(IBuffer buffer, uint payloadLength);
+    }
+}

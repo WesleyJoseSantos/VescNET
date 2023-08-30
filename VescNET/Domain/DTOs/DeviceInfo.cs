0808f7c4cce2f98e62230e88b8c75e3877f41859
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-08-30 18:06:53 -0300

    feat: create DeviceInfo DTO

---
+++ VescNET/Domain/DTOs/DeviceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VescNET.Domain.DTOs
+{
+    public class DeviceInfo
+    {
+        public string FirmwareVersion { get; set; }
+        
+        public string HardwareVersion { get; set; }
+
+        public byte[] Uuid { get; set; }
+    }
+}

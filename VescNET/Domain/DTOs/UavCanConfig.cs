7329d50c9a0785f19a6f527ad360fcc8cd96ca98
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-09-08 09:59:01 -0300

    feat: create DTO

---
+++ VescNET/Domain/DTOs/UavCanConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace VescNET.Domain.DTOs
+{
+    public class UavCanConfig
+    {
+        public byte EscIndex { get; set; }
+        public byte RawThrottleMode { get; set; }
+        public float RawRpmMax { get; set; }
+        public byte StatusCurrentMode { get; set; }
+    }
+}

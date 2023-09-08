c5ec4ddc85e926f13fa786b12d65453ec252824d
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-09-08 09:57:31 -0300

    feat: create DTO

---
+++ VescNET/Domain/DTOs/CanConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VescNET.Domain.Enums;
+
+namespace VescNET.Domain.DTOs
+{
+    public class CanConfig
+    {
+        public ushort StatusMessageMode { get; set; }
+        public byte StatusRateHz { get; set; }
+        public CanBaud BaudRate { get; set; }
+        public byte CanMode { get; set; }
+        public UavCanConfig Uav { get; set; }
+
+        public CanConfig() 
+        {
+            Uav = new UavCanConfig();
+        }
+    }
+}

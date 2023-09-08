e4e54960bda105fb15974ec5ba801715bd1ff8d1
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-09-08 09:58:41 -0300

    feat: create DTO

---
+++ VescNET/Domain/DTOs/PasConfig.cs
@@ -0,0 +1,19 @@
+﻿using VescNET.Domain.Enums;
+
+namespace VescNET.Domain.DTOs
+{
+    public class PasConfig
+    {
+        public PasControlType CtrlType { get; set; }
+        public PasSensorType SensorType { get; set; }
+        public float CurrentScaling { get; set; }
+        public float PedalRpmStart { get; set; }
+        public float PedalRpmEnd { get; set; }
+        public bool InvertPedalDirection { get; set; }
+        public ushort Magnets { get; set; }
+        public bool UseFilter { get; set; }
+        public float RampTimePos { get; set; }
+        public float RampTimeNeg { get; set; }
+        public uint UpdateRateHz { get; set; }
+    }
+}

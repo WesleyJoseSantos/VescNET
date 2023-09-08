6d25fb8f7a63fca1b1e3d2957a71bacca0abf85d
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-09-08 09:57:22 -0300

    feat: create DTO

---
+++ VescNET/Domain/DTOs/BmsConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VescNET.Domain.Enums;
+
+namespace VescNET.Domain.DTOs
+{
+    public class BmsConfig
+    {
+        public BmsType Type { get; set; }
+        public byte LimitMode { get; set; }
+        public float TLimitStart { get; set; }
+        public float TLimitEnd { get; set; }
+        public float SocLimitStart { get; set; }
+        public float SocLimitEnd { get; set; }
+        public BmsFwdCanMode FwdCanMode { get; set; }
+    }
+}

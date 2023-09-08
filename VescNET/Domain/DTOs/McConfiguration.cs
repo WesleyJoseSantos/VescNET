88e900e3c44e8022ef4e67b831b0f28e9dbaef39
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-09-08 09:58:33 -0300

    refact: refact DTO

--- VescNET/Domain/DTOs/McConfiguration.cs
+++ VescNET/Domain/DTOs/McConfiguration.cs
@@ -42,12 +42,13 @@ namespace VescNET.Domain.DTOs
         public float LWattMin { get; set; }
 
         // Overridden limits (Computed during runtime)
-        public float LoCurrentMax { get; set; }
-        public float LoCurrentMin { get; set; }
-        public float LoInCurrentMax { get; set; }
-        public float LoInCurrentMin { get; set; }
-        public float LoCurrentMotorMaxNow { get; set; }
-        public float LoCurrentMotorMinNow { get; set; }
+        public float LCurrentMaxScale { get; set; }
+        public float LCurrentMinScale { get; set; }
+        public float LDutyStart { get; set; }
+        //public float LoInCurrentMax { get; set; }
+        //public float LoInCurrentMin { get; set; }
+        //public float LoCurrentMotorMaxNow { get; set; }
+        //public float LoCurrentMotorMinNow { get; set; }
 
         // Sensorless (bldc)
         public float SlMinErpm { get; set; }
@@ -70,22 +71,33 @@ namespace VescNET.Domain.DTOs
         public float FocEncoderOffset { get; set; }
         public bool FocEncoderInverted { get; set; }
         public float FocEncoderRatio { get; set; }
+        public float FocEncoderSinGain{ get; set; }
+        public float FocEncoderCosGain { get; set; }
+        public float FocEncoderSinOffset { get; set; }
+        public float FocEncoderCosOfset { get; set; }
+        public float FocEncoderSinCosFilterConstant { get; set; }
         public float FocMotorL { get; set; }
+        public float FocMotorLdLqDiff { get; set; }
         public float FocMotorR { get; set; }
         public float FocMotorFluxLinkage { get; set; }
         public float FocObserverGain { get; set; }
         public float FocObserverGainSlow { get; set; }
-        public float FocPllKp { get; set; }
-        public float FocPllKi { get; set; }
+        public float FocObserverOffset { get; set; }
+        public float FocSpeedTrackerKp { get; set; }
+        public float FocSpeedTrackerKi { get; set; }
         public float FocDutyDowmrampKp { get; set; }
         public float FocDutyDowmrampKi { get; set; }
         public float FocOpenloopRpm { get; set; }
+        public float FocOpenloopRpmLow { get; set; }
+        public float FocDGainScaleStart { get; set; }
+        public float FocDGainScaleMaxMod { get; set; }
         public float FocSlOpenloopHyst { get; set; }
+        public float FocSlOpenloopTimeLock { get; set; }
+        public float FocSlOpenloopTimeRamp { get; set; }
         public float FocSlOpenloopTime { get; set; }
-        public float FocSlDCurrentDuty { get; set; }
-        public float FocSlDCurrentFactor { get; set; }
         public McFocSensorMode FocSensorMode { get; set; }
         public byte[] FocHallTable { get; set; }
+        public float FocHallInterpErpm { get; set; }
         public float FocSlErpm { get; set; }
         public bool FocSampleV0V7 { get; set; }
         public bool FocSampleHighCurrent { get; set; }
@@ -93,41 +105,105 @@ namespace VescNET.Domain.DTOs
         public bool FocTempComp { get; set; }
         public float FocTempCompBaseTemp { get; set; }
         public float FocCurrentFilterConst { get; set; }
+        public byte FocCcDecoupling { get; set; }
+        public byte FocObserverType { get; set; }
+        public float FocHfiVoltageStart { get; set; }
+        public float FocHfiVoltageRun { get; set; }
+        public float FocHfiVoltageMax { get; set; }
+        public float FocHfiGain { get; set; }
+        public float FocHfiHyst { get; set; }
+        public float FocSlErpmHfi { get; set; }
+        public float FocHfiStartSamples { get; set; }
+        public float FocHfiObsOvrSec { get; set; }
+        public byte FocHfiSamples { get; set; }
+        public byte FocOffsetsCalOnBoot { get; set; }
+        public float[] FocOffsetsCurrent { get; set; }
+        public float[] FocOffsetsVoltage { get; set; }
+        public float[] FocOffsetsVoltageUndriven { get; set; }
+        public bool FocPhaseFilterEnable { get; set; }
+        //public bool FocPhaseFilterDisableFault { get; set; }
+        public float FocPhaseFilterMaxErpm { get; set; }
+        public byte FocMtpaMode { get; set; }
+        public float FocFwCurrentMax { get; set; }
+        public float FocFwDutyStart { get; set; }
+        public float FocFwRampTime { get; set; }
+        public float FocFwQCurrentFactor { get; set; }
+        public byte FocSpeedSoure { get; set; }
 
         // Speed PID
+        public byte SPidLoopRate { get; set; }
         public float SPidKp { get; set; }
         public float SPidKi { get; set; }
         public float SPidKd { get; set; }
         public float SPidKdFilter { get; set; }
         public float SPidMinErpm { get; set; }
         public bool SPidAllowBraking { get; set; }
+        public float SPidRampErpmsS { get; set; }
+
+        // GPDrive
+        public int GpdBufferNotifyLeft { get; set; }
+        public int GpdBufferInterpol { get; set; }
+        public float GpdCurrentFilterConst { get; set; }
+        public float GpdCurrentKp { get; set; }
+        public float GpdCurrentKi { get; set; }
 
         // Pos PID
         public float PPidKp { get; set; }
         public float PPidKi { get; set; }
         public float PPidKd { get; set; }
+        public float PPidKdProc { get; set; }
         public float PPidKdFilter { get; set; }
         public float PPidAngDiv { get; set; }
+        public float PPidGainDecAngle { get; set; }
+        public float PPidOffset { get; set; }
 
         // Current controller
         public float CcStartupBoostDuty { get; set; }
         public float CcMinCurrent { get; set; }
-        public float CcGain { get; set; }
+        public float CcControllerGain { get; set; }
         public float CcRampStepMax { get; set; }
 
         // Misc
         public int MFaultStopTimeMs { get; set; }
-        public float MDutyRampStep { get; set; }
-        public float MCurrentBackoffGain { get; set; }
-        public uint MEncoderCounts { get; set; }
-        public SensorPortMode MSensorPortMode { get; set; }
-        public bool MInvertDirection { get; set; }
-        public Drv8301OcMode MDrv8301OcMode { get; set; }
-        public int MDrv8301OcAdj { get; set; }
-        public float MBldcFSwMin { get; set; }
-        public float MBldcFSwMax { get; set; }
-        public float MDcFSw { get; set; }
-        public float MNtcMotorBeta { get; set; }
-        public OutAuxMode MOutAuxMode { get; set; }
+        public float MDutyRampStep {get; set;}
+        public float MCurrentBackoffGain {get; set;}
+        public uint MEncoderCounts {get; set;}
+        public float MEncoderSinOffset {get; set;}
+        public float MEncoderSinAmp {get; set;}
+        public float MEncoderCosOffset {get; set;}
+        public float MEncoderCosAmp {get; set;}
+        public float MEncoderSincosFilterConstant {get; set;}
+        public float MEncoderSincosPhaseCorrection {get; set;}
+        public SensorPortMode MSensorPortMode {get; set;}
+        public bool MInvertDirection {get; set;}
+        public Drv8301OcMode MDrv8301OcMode {get; set;}
+        public int MDrv8301OcAdj {get; set;}
+        public float MBldcFSwMin {get; set;}
+        public float MBldcFSwMax {get; set;}
+        public float MDcFSw {get; set;}
+        public float MNtcMotorBeta {get; set;}
+        public OutAuxMode MOutAuxMode {get; set;}
+        public TempSensorType MMotorTempSensType {get; set;}
+        public float MPtcMotorCoeff {get; set;}
+        public int MHallExtraSamples {get; set;}
+        public int MBattFilterConst {get; set;}
+        public float MNtcxPtcxTempBase {get; set;}
+        public float MNtcxPtcxRes {get; set;}
+
+        // Setup info
+        public byte SiMotorPoles {get; set;}
+        public float SiGearRatio {get; set;}
+        public float SiWheelDiameter {get; set;}
+        public BatteryType SiBatteryType {get; set;}
+        public int SiBatteryCells {get; set;}
+        public float SiBatteryAh {get; set;}
+        public float SiMotorNlCurrent {get; set;}
+
+        public BmsConfig Bms {get; set;}
+
+        public McConfiguration()
+        {
+            Bms = new BmsConfig();
+        }
     }
 }
